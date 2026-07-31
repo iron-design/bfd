9ce512165d62c9ac14f4782911d302a33662b209
philip2607 <designormal@naver.com>
2026-07-31 22:41:05 +0900

    init: SwiftUI macOS app project setup

---
+++ RecoveryPomodoro/RecoveryPomodoro/RecoveryPomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoveryPomodoroApp.swift
+//  RecoveryPomodoro
+//
+//  Created by dift on 7/31/26.
+//
+
+import SwiftUI
+
+@main
+struct RecoveryPomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecoveryPomodoro/RecoveryPomodoroTests/RecoveryPomodoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecoveryPomodoroTests.swift
+//  RecoveryPomodoroTests
+//
+//  Created by dift on 7/31/26.
+//
+
+import Testing
+@testable import RecoveryPomodoro
+
+struct RecoveryPomodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
